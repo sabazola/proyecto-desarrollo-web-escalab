0a0961b33b669ff03d27be2167b5113b677d0f28
sabazola <sabazola@gmail.com>
2021-01-12 23:45:33 -0300

    tercer commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,9 @@
 // TRANSFORMAR DOLARES A PESOS CHILENOS
 function dolar_peso() {
     var n1 = document.getElementById('dolar').value;
-    var mult = n1 * 711.12;
-    if (isNaN(mult)){
-        alert("Debe ingresar un valor")
+    var mult = parseInt(n1) * 711.12;
+    if (isNaN(mult) || (n1<=0) ){
+        alert("Ingrese un valor mayor a cero")
     }
     else{
     alert(n1 + " dolares son: "+mult.toFixed(3)+" pesos chilenos.")
@@ -12,15 +12,15 @@ function dolar_peso() {
 // TRANSFORMAR PESOS CHILENOS A DOLARES
 function peso_dolar() {
     var n1 = document.getElementById('peso').value;
-    var mult = n1 / 711.12;
-    if (isNaN(mult)){
-        alert("Debe ingresar un valor")
+    var mult = parseInt(n1) / 711.12;
+    if (isNaN(mult) || (n1<=0) ){
+        alert("Ingrese un valor mayor a cero")
     }
     else{
     alert(n1 + " pesos chilenos son: "+mult.toFixed(3)+" dolares.")
     }
 }
-// MENSAJE REGISTRO EXITOSO
-function registroexitoso(){
+// VALIDAR FORMULARIO REGISTRO
+function valida_registro(){
     alert("Gracias por registrarte, pronto nos pondremos en contacto con usted.");
 }
